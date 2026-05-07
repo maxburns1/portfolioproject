28fb7ed229b11bd13832b68619ebf621ff3ba5c8
Max Burns <max_burns1@baylor.edu>
2026-05-07 18:15:39 -0500

    configure project for render

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Add this line temporarily to import your data
+python manage.py loaddata db_backup.json
